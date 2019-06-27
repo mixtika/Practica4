83213c35b3ca2b9f96be82f332f7c8417f19d69d
Silvia Rosario Coro <mirian.mcp19@gmail.com>
2019-06-27 10:41:41 -0400

    Fin
    Fin

--- app/src/main/java/com/isoft/practica4/MainActivity.java
+++ app/src/main/java/com/isoft/practica4/MainActivity.java
@@ -2,12 +2,42 @@ package com.isoft.practica4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText t1,t2,t3,tres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1 = (EditText) findViewById(R.id.t1);
+        t2 = (EditText) findViewById(R.id.t2);
+        t3 = (EditText) findViewById(R.id.t3);
     }
+
+
+
+    public void calcular(View view) {
+        //Toast.makeText(this,"hola",Toast.LENGTH_SHORT).show();
+        int n1=Integer.parseInt(t1.getText().toString());
+        int n2=Integer.parseInt(t2.getText().toString());
+        int n3=Integer.parseInt(t3.getText().toString());
+        tres = (EditText) findViewById(R.id.tres);
+        if (n1 > n2) {
+            if (n1 > n3) {
+                tres.setText(n1+"");
+            } else {
+                tres.setText(n3+"");
+            }
+        } else if (n2 > n3) {
+            tres.setText(n2+"");
+        } else {
+            tres.setText(n3+"");
+        }
+    }
+
 }
